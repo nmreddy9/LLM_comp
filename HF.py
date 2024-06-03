9d7959b96ef0cb8d3848fbf46ac364518d648983
NMReddy <85029859+nmreddy9@users.noreply.github.com>
2024-06-03 13:20:51 +0530

    Update HF.py
    code changes

--- HF.py
+++ HF.py
@@ -6,9 +6,11 @@ from langchain import PromptTemplate, LLMChain
 
 def summarize_text(input_text):
 
-    repo_id = "facebook/bart-large-cnn"
+    repo_id = "pszemraj/led-large-book-summary"
+    # hfid = "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"
     hfid = "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"
-    llm = HuggingFaceHub(huggingfacehub_api_token=hfid, repo_id=repo_id, model_kwargs={ "min_length":int(len(input_text)/1.5)})
+
+    llm = HuggingFaceHub(huggingfacehub_api_token=hfid, repo_id=repo_id, model_kwargs={ "max_length":int(len(input_text)), "temperature":0.7})
 
     template = """Question : {question} \n
                 Answer : Let me give you detailed answer"""
@@ -20,16 +22,10 @@ def summarize_text(input_text):
     out = chian.run(input_text)
     return out
 
-def classify_text(input_text):
-    classifier = pipeline("sentiment-analysis")
-    classification = classifier(input_text)[0]['label']
-    return classification
-
-def rephrase_text(input_text):
-
-    repo_id = "humarin/chatgpt_paraphraser_on_T5_base"
+def text_generator(input_text):
+    repo_id = "tiiuae/falcon-7b-instruct"
     hfid = "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"
-    llm = HuggingFaceHub(huggingfacehub_api_token=hfid, repo_id=repo_id, model_kwargs={"min_length":int(len(input_text) - len(input_text)*0.3), "max_length":int(len(input_text)),  "temperature":0.7})
+    llm = HuggingFaceHub(huggingfacehub_api_token=hfid, repo_id=repo_id, model_kwargs={ "min_length":400, "max_length":1000,  "temperature":0.9})
 
 
     template = """Question : {question} \n
@@ -41,11 +37,12 @@ def rephrase_text(input_text):
 
     out = chian.run(input_text)
     return out
+
 
 
 def LLMModel(input_text):
     repo_id = "tiiuae/falcon-7b-instruct"
-    hfid = "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"
+    hfid = "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"
     llm = HuggingFaceHub(huggingfacehub_api_token=hfid, repo_id=repo_id)
 
     template = """Question : {question} \n
@@ -59,52 +56,31 @@ def LLMModel(input_text):
     return out
 
 
+
+
 # Streamlit app layout
 st.title("ASK ME - IVY")
 
 # Sidebar with task selection
-selected_task = st.sidebar.selectbox("Select NLP Task:", [ "Code", "Summarization", "Text Classification", "Text Rephrasing"])
+selected_task = st.sidebar.selectbox("Select NLP Task:", [ "Code", "Bookish Summary",  "Text Generation"])
 
 # Input text area
 input_text = st.text_area("Enter Text:")
 
 # Buttons to trigger tasks
 if st.button("Process"):
-    if selected_task == "Summarization" and input_text:
-        st.subheader("Summary:")
+    if selected_task == "Bookish Summary" and input_text:
         result = summarize_text(input_text)
-        try:
-            st.write(result)
-        except:
-            st.write("Please try after one hour")
+        st.write(result)
 
-
-
-    elif selected_task == "Text Classification" and input_text:
-        st.subheader("Classification:")
-        result = classify_text(input_text)
-        try:
-            st.write(f"The text is classified as: {result}")
-        except:
-            st.write("Please try after one hour")
-        
-    elif selected_task == "Text Rephrasing" and input_text:
-        st.subheader("Text Rephrasing:")
-        result = rephrase_text(input_text)
-        try:
-            st.write(result)
-        except:
-            st.write("Please try after one hour")
-        
+    elif selected_task == "Text Generation" and input_text:
+        result = text_generator(input_text)
+        st.write(result)
 
     elif selected_task == "Code" and input_text:
-        st.subheader("Code:")
         result = LLMModel(input_text)
-        try:
-            st.write(result)
-        except:
-            st.write("Please try after one hour")
-        
+        st.write(result)
+
 
     else:
         st.info("Please enter text and select a task from the sidebar.")
